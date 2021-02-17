0d67a4a5449998c654bbe9623948b07a333a4a33
Crawford Leeds <crawford@crawfordleeds.com>
2021-02-16 19:46:21 -0700

    start model

---
+++ btcpay_django/models.py
@@ -0,0 +1,42 @@
+import pickle
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from crawfish.models import BaseModel
+from django.conf import settings
+from btcpay import BTCPayClient as BTCPayClient_sdk
+
+
+class BtcpayClient(BaseModel):
+
+    class Meta:
+        verbose_name = "BTCPay Client"
+        verbose_name_plural = "BTCPay Clients"
+
+    client = models.BinaryField(_("Pickled Client"), blank=True)
+    pairing_code = models.CharField(_("Pairing Code"), help_text="One time use pairing code",  max_length=50)
+    host = models.URLField()
+
+    def save(self, *args, **kwargs):
+        """
+        Override the save method to create a new client when creating a new record.
+        """
+        created = self.pk is None
+
+        if created:
+            assert self.pairing_code is not None, "You must add a pairing_code to created a new BtcpayClient record"
+            if not self.host:
+                assert getattr(settings, "BTCPAYSERVER_HOST", None) is not None, "ADD BTCPAYSERVER_HOST to your settings or save() with non-None host"
+                client = BTCPayClient_sdk.create_client(
+                    host=settings.BTCPAYSERVER_HOST, code=self.pairing_code
+                )
+            else:
+                client = BTCPayClient_sdk.create_client(host=self.host, code=self.pairing_code)
+
+            self.client = pickle.dumps(client)
+        super().save(*args, **kwargs)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return f"BtcpayClient(host: '{self.host}')"
